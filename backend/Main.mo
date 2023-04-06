be5fe08bfc5bb120c397917f00493a36f33f8448
Zane <alessandro.demaio@virgilio.it>
2023-04-06 18:07:17 +0200

    asset storage canister

--- backend/Main.mo
+++ backend/Main.mo
@@ -1,18 +1,31 @@
-actor {
-  stable var counter = 0;
+import Array "mo:base/Array";
+import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
+import Nat8 "mo:base/Nat8";
+import Principal "mo:base/Principal";
 
-  // Get the current count
-  public query func get() : async Nat {
-    counter;
-  };
+shared ({ caller }) actor class Main() = this {
+  // stable var counter = 0;
+  // type test = (Nat8, Nat8, Nat8, Principal, Principal);
+  // stable let matrix : [test] = Array.freeze(Array.init<test>(256 * 256, (0, 0, 0, caller, caller)));
 
-  // Increment the count by one
-  public func inc() {
-    counter += 1;
-  };
+  // // Get the current count
+  // public query func get() : async Nat {
+  //   counter;
+  // };
 
-  // Add `n` to the current count
-  public func add(n : Nat) {
-    counter += n;
-  };
+  // public query func getArray() : async [test] {
+  //   //Debug.print(Array.sizeof(matrix));
+  //   matrix;
+  // };
+
+  // // Increment the count by one
+  // public func inc() {
+  //   counter += 1;
+  // };
+
+  // // Add `n` to the current count
+  // public func add(n : Nat) {
+  //   counter += n;
+  // };
 };
--- src/App.jsx
+++ src/App.jsx
@@ -5,169 +5,142 @@ import motokoLogo from './assets/motoko_moving.png';
 import motokoShadowLogo from './assets/motoko_shadow.png';
 import reactLogo from './assets/react.svg';
 import { backend } from './declarations/backend';
+import { storage } from './declarations/storage';
 
-const pixel = {
-  r: 0,
-  g: 0,
-  b: 0
+
+async function getUint8Array(file) {
+  const arrayBuffer = await new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = function () {
+      resolve(reader.result);
+    };
+    reader.onerror = function () {
+      reject(reader.error);
+    };
+    reader.readAsArrayBuffer(file);
+  });
+  const uint8Array = new Uint8Array(arrayBuffer);
+  console.log('Uint8Array:', uint8Array);
+  return uint8Array
 }
-
-// const canvas = document.getElementById("myCanvas");
-// const ctx = canvas.getContext("2d");
-// let scale = 1;
-let translateX = 0;
-let translateY = 0;
-
-// canvas.addEventListener("wheel", function (event) {
-//   event.preventDefault();
-//   if (event.deltaY < 0) {
-//     // zoom in
-//     scale *= 1.1;
-//   } else {
-//     // zoom out
-//     scale /= 1.1;
-//   }
-//   draw();
-// });
-
-// let isDragging = false;
-// let lastX = 0;
-// let lastY = 0;
-
-// canvas.addEventListener("mousedown", function (event) {
-//   isDragging = true;
-//   lastX = event.clientX;
-//   lastY = event.clientY;
-// });
-
-// canvas.addEventListener("mousemove", function (event) {
-//   if (isDragging) {
-//     const deltaX = event.clientX - lastX;
-//     const deltaY = event.clientY - lastY;
-//     translateX += deltaX / scale;
-//     translateY += deltaY / scale;
-//     lastX = event.clientX;
-//     lastY = event.clientY;
-//     draw();
-//   }
-// });
-
-// canvas.addEventListener("mouseup", function (event) {
-//   isDragging = false;
-// });
-
-// canvas.addEventListener("click", function (event) {
-//   const x = event.clientX;
-//   const y = event.clientY;
-//   highlightPixel(x, y);
-// });
-
-// function draw() {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   ctx.save();
-//   ctx.scale(scale, scale);
-//   ctx.translate(translateX, translateY);
-//   // draw the canvas content here
-//   ctx.restore();
-// }
-
-// function highlightPixel(x, y) {
-//   const unscaledX = (x - translateX) / scale;
-//   const unscaledY = (y - translateY) / scale;
-//   const imageData = ctx.getImageData(unscaledX, unscaledY, 1, 1);
-//   const pixel = imageData.data;
-
-//   ctx.strokeStyle = "red";
-//   ctx.strokeRect(unscaledX - 5, unscaledY - 5, 10, 10);
-// }
-const MAX_ZOOM = 5
-const MIN_ZOOM = 0.1
-const SCROLL_SENSITIVITY = 0.0005
 
 function App() {
   const [count, setCount] = useState(0);
   const [loading, setLoading] = useState(false);
-  const [matrix, setMatrix] = useState();
-  const [context, setContext] = useState(null);
-  const [tick, setTick] = useState(true);
-  const [scale, setScale] = useState(1);
-  const canvasRef = useRef(null);
-  const imgRef = useRef(null);
+  const [file, setFile] = useState(null);
+  const [dragging, setDragging] = useState(false);
+  const [error, setError] = useState(null);
+  const [uploaded, setUploaded] = useState(null);
 
-  const zoom = (e) => {
-    //e.preventDefault();
-    console.log("zoom event")
-    let zoomAmount = e.deltaY * SCROLL_SENSITIVITY
-    let cameraZoom = MIN_ZOOM;
-    setScale(oldZoom => {
-      cameraZoom = oldZoom + zoomAmount
-      cameraZoom = Math.min(cameraZoom, MAX_ZOOM)
-      cameraZoom = Math.max(cameraZoom, MIN_ZOOM)
-      return cameraZoom
-    })
+  function handleFileUpload(event) {
+    const selectedFile = event.target.files[0];
+    console.log(selectedFile)
+    validateFile(selectedFile);
   }
 
+  function handleDrop(event) {
+    handleFileUpload(event)
+    setDragging(false);
+  }
 
+  function handleDragOver(event) {
+    event.preventDefault();
+    setDragging(true);
+  }
 
-  const renderCanvas = () => {
-    // Draw the ImageData object onto the canvas
-    console.log("renderCanvas")
-    if (matrix && context) {
-      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-      context.save();
-      context.scale(scale, scale);
-      context.putImageData(matrix, 0, 0);
-      context.restore();
-      console.log(scale)
+  function handleDragEnter(event) {
+    event.preventDefault();
+    setDragging(true);
+  }
+
+  function handleDragLeave(event) {
+    event.preventDefault();
+    setDragging(false);
+  }
+
+  function validateFile(file) {
+    const maxSize = 1024 * 1024 * 10; // 10 MB
+    if (file.size > maxSize) {
+      setError('File size exceeds 10 MB');
+      setFile(null);
+    } else {
+      setError(null);
+      setFile(file);
     }
+  }
+
+  const uploadImage = async () => {
+    console.log(file)
+    let chunk_ids = [];
+    let batch_id = Math.random().toString(36).substring(2, 7);
+
+    const uploadChunk = async ({ chunk, order }) => {
+      return storage.create_chunk(batch_id, chunk, order);
+    };
+    const asset_unit8Array = await getUint8Array(file)
+    console.log(asset_unit8Array)
+    const promises = [];
+    const chunkSize = 2000000;
+
+    for (
+      let start = 0, index = 0;
+      start < asset_unit8Array.length;
+      start += chunkSize, index++
+    ) {
+      const chunk = asset_unit8Array.slice(start, start + chunkSize);
+      console.log(chunk)
+      promises.push(
+        uploadChunk({
+          chunk,
+          order: index,
+        })
+      );
+    }
+
+    chunk_ids = await Promise.all(promises);
+
+
+    const asset_filename = file.name;
+    // const response = await fetch(file);
+    // const blob = await response.blob();
+    // const type = blob.type;
+    const asset_content_type = file.type
+    const { ok: asset_id } = await storage.commit_batch(
+      batch_id,
+      chunk_ids,
+      {
+        filename: asset_filename,
+        content_encoding: "gzip",
+        content_type: asset_content_type,
+      }
+    );
+
+    const { ok: asset } = await storage.get(asset_id);
+    console.log(asset);
+    setUploaded(asset.url)
+  }
+
+  const mintNft = async () => {
 
   }
 
-  useEffect(() => {
-    var canvas = canvasRef.current
-    canvas.width = 1024;
-    canvas.height = 1024;
-    var context = canvas.getContext("2d");
-    setContext(context)
-    // context.scale(scale, scale);
-    var imageData = context.createImageData(canvas.width, canvas.height);
-    for (var i = 0; i < imageData.data.length; i += 4) {
-      var x = (i / 4) % canvas.width;
-      var y = Math.floor(i / (4 * canvas.height));
-      let ran = Math.floor(Math.random() * 256);
-      let ran1 = Math.floor(Math.random() * 256);
-      let ran2 = Math.floor(Math.random() * 256);
-      imageData.data[i] = ran; // Red channel
-      imageData.data[i + 1] = ran1; // Green channel
-      imageData.data[i + 2] = ran2; // Blue channel
-      imageData.data[i + 3] = 255; // Alpha channel
-    }
-
-    setMatrix(imageData);
-    console.log(imageData);
-
-    //setTick(oltTick => oltTick != oltTick)
-  }, []);
-
-  // useEffect(() => {
-  //   renderCanvas();
-  //   const wait = setInterval(() => {
-  //     setTick(oldTick => oldTick = !oldTick)
-  //   }, 100)
-
-  //   return () => {
-  //     clearInterval(wait);
-  //   };
-  // }, [tick]);
-
-  useEffect(() => {
-    renderCanvas();
-  }, [zoom]);
 
   return (
     <div className="">
-      <canvas className="hidden" id="canvas" ref={canvasRef} width={1024} height={1024} onWheel={zoom}></canvas>
-      <Canvas canvasHeight={1024} canvasWidth={1024} />
+      <button onClick={uploadImage}>Test upload</button>
+      <button onClick={mintNft}>Mint NFT</button>
+      <div
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        className={dragging ? 'dragging' : ''}
+      >
+        <input type="file" onChange={handleFileUpload} />
+        {error && <p>{error}</p>}
+        {file && <p>Selected file: {file.name}</p>}
+        {uploaded && <img src={uploaded}></img>}
+      </div>
     </div>
   );
 }
